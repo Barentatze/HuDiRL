906c2a2870e3b56eb35632770546e9db1328122b
Barentatze <chenjifeng2002@icloud.com>
2025-04-21 18:45:41 +1000

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -3,17 +3,17 @@
 # Download and install Miniconda
 MINICONDA_INSTALLER=Miniconda3-latest-Linux-x86_64.sh
 wget https://repo.anaconda.com/miniconda/$MINICONDA_INSTALLER -O ~/miniconda.sh
-#bash ~/miniconda.sh -b -p $HOME/miniconda
-#export PATH="$HOME/miniconda/bin:$PATH"
+bash ~/miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
 
 # Get the parent directory of the current working directory
-PARENT_DIR="$(cd .. && pwd)"
+#PARENT_DIR="$(cd .. && pwd)"
 
 # Install Miniconda to $PARENT_DIR/miniconda
-bash ~/miniconda.sh -b -p "$PARENT_DIR/miniconda"
+#bash ~/miniconda.sh -b -p "$PARENT_DIR/miniconda"
 
 # Add to PATH
-export PATH="$PARENT_DIR/miniconda/bin:$PATH"
+#export PATH="$PARENT_DIR/miniconda/bin:$PATH"
 
 # Initialize Conda and create a new environment
 conda init
